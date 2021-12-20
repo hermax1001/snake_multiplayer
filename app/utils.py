e11e1b7caadc4854c94344f50a4d0108900094c5
herman_max <dmgerman102@gmail.com>
2021-12-20 23:49:10 +0300

    add styles and district opposite directions

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 from app.const import Direction
 
 
-def is_opposite_direction(current, new: Direction) -> bool:
+def is_opposite_direction(current: Direction, new: Direction) -> bool:
     """Return True if the direction is opposite"""
     return {current, new} in ({Direction.UP, Direction.DOWN}, {Direction.RIGHT, Direction.LEFT})
